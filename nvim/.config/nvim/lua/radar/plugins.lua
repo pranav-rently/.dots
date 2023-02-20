913b57c0b4ce6c26d9e0c66ef7aee6f1b94ddd8f
Pranav RK <pranav.r@rently.com>
2023-02-20 14:53:44 +0530

    dotfiles commit

--- nvim/.config/nvim/lua/radar/plugins.lua
+++ nvim/.config/nvim/lua/radar/plugins.lua
@@ -108,6 +108,13 @@ return packer.startup(function(use)
   -- Personal Wiki
   use "vimwiki/vimwiki"
 
+  -- Markdown previewer
+  -- install without yarn or npm
+  --[[ use({ ]]
+  --[[   "iamcco/markdown-preview.nvim", ]]
+  --[[   run = function() vim.fn["mkdp#util#install"]() end, ]]
+  --[[ }) ]]
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
