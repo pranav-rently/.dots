768ec27696c522d3bf9e381e98b54dd4650778e1
Pranav RK <pranav.r@rently.com>
2023-07-01 08:21:53 +0530

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,10 +26,19 @@ local yank_group = augroup('HighlightYank', {})
 autocmd('TextYankPost', {
   group = yank_group,
   pattern = '*',
-  callback = function ()
+  callback = function()
     vim.highlight.on_yank({
       higroup = 'IncSearch',
       timeout = 75,
     })
+  end
+})
+
+-- Format while saving
+autocmd('BufWritePre', {
+  --[[ group = yank_group, ]]
+  pattern = '*',
+  callback = function()
+    vim.lsp.buf.format()
   end
 })
