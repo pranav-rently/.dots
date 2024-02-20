fd1464365f96695653e328bdd229246676780463
Pranav RK <pranav.r@rently.com>
2024-02-20 20:14:41 +0530

    neovim: chore updates

--- nvim/.config/nvim/lua/radar/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/radar/plugins/colorscheme.lua
@@ -12,40 +12,44 @@ return {
       vim.cmd.colorscheme("tokyonight")
     end,
   },
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   opts = {
-  --     integrations = {
-  --       cmp = true,
-  --       gitsigns = true,
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       markdown = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       nvimtree = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       treesitter_context = true,
-  --       which_key = true,
-  --     },
-  --   },
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       transparent_background = true,
-  --     })
-  --     -- load the colorscheme here
-  --     vim.cmd.colorscheme("catppuccin-frappe")
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        term_colors = true,
+        styles = {
+          keywords = { "italic" },
+        },
+      })
+      -- load the colorscheme here
+      -- vim.cmd.colorscheme("catppuccin-frappe")
+    end,
+  },
 }
