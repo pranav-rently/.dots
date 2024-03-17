233890c284774bf0a976450707c558c8cd46e63b
Pranav RK <pranav.r@rently.com>
2024-03-17 22:14:38 +0530

    nvim: fidget config

--- nvim/.config/nvim/lua/radar/plugins/fidget.lua
+++ nvim/.config/nvim/lua/radar/plugins/fidget.lua
@@ -2,6 +2,20 @@ return {
   "j-hui/fidget.nvim",
   tag = "v1.0.0",
   opts = {
-    -- options
+    progress = {
+      ignore_done_already = true,
+      ignore_empty_message = true,
+      display = {
+        progress_icon = {
+          pattern = "earth",
+          period = 1,
+        },
+      },
+    },
+    notification = {
+      view = {
+        stack_upwards = false,
+      },
+    },
   },
 }
--- nvim/.config/nvim/lua/radar/plugins/mini.lua
+++ nvim/.config/nvim/lua/radar/plugins/mini.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "echasnovski/mini.pairs",
-    version = "*",
-    config = function()
-      require("mini.pairs").setup({})
-    end,
+    -- "echasnovski/mini.pairs",
+    -- version = "*",
+    -- config = function()
+    --   require("mini.pairs").setup({})
+    -- end,
   },
   {
     "echasnovski/mini.surround",
