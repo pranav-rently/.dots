1b031bcdb4b9b394cc8d1edbaae264e4d9754994
Pranav RK <pranav.r@rently.com>
2024-03-23 16:53:03 +0530

    nvim: updates

--- nvim/.config/nvim/lua/radar/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/radar/plugins/colorscheme.lua
@@ -16,43 +16,43 @@ return {
     end,
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        term_colors = true,
-        styles = {
-          keywords = { "italic" },
-        },
-      })
-      -- load the colorscheme here
-      -- vim.cmd.colorscheme("catppuccin")
-    end,
+    -- "catppuccin/nvim",
+    -- name = "catppuccin",
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- opts = {
+    --   integrations = {
+    --     cmp = true,
+    --     gitsigns = true,
+    --     lsp_trouble = true,
+    --     mason = true,
+    --     markdown = true,
+    --     mini = true,
+    --     native_lsp = {
+    --       enabled = true,
+    --       underlines = {
+    --         errors = { "undercurl" },
+    --         hints = { "undercurl" },
+    --         warnings = { "undercurl" },
+    --         information = { "undercurl" },
+    --       },
+    --     },
+    --     nvimtree = true,
+    --     telescope = true,
+    --     treesitter = true,
+    --     treesitter_context = true,
+    --     which_key = true,
+    --   },
+    -- },
+    -- config = function()
+    --   require("catppuccin").setup({
+    --     transparent_background = true,
+    --     term_colors = true,
+    --     styles = {
+    --       keywords = { "italic" },
+    --     },
+    --   })
+    --   -- load the colorscheme here
+    --   vim.cmd.colorscheme("catppuccin")
+    -- end,
   },
 }
--- nvim/.config/nvim/lua/radar/plugins/mini.lua
+++ nvim/.config/nvim/lua/radar/plugins/mini.lua
@@ -23,4 +23,27 @@ return {
       })
     end,
   },
+  {
+    "echasnovski/mini.indentscope",
+    version = "*",
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "nvimtree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
 }
